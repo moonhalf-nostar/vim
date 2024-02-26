1d834af8f12ca7062f9e15abc95174ce169bd157
moonhalf-nostar <whz-2000@qq.com>
2024-02-26 11:14:07 +0800

    add a script to install vim-plug in linux.

---
+++ install-vimplug.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://github.com/junegunn/vim-plug?tab=readme-ov-file#vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
