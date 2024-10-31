61f5c740aa1a25638b89d8b40e42d6f3732151fb
ryan <whz-2000@qq.com>
2024-11-01 01:14:18 +0800

    add neovim vim-plug install cmd in comment

--- install-vimplug.sh
+++ install-vimplug.sh
@@ -3,3 +3,7 @@
 # https://github.com/junegunn/vim-plug?tab=readme-ov-file#vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# https://github.com/junegunn/vim-plug?tab=readme-ov-file#neovim
+# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+#        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
